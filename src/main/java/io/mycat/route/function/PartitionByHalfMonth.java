345a8c06ef33f6db736700f2216a41716f56dccb
dengly <denglylove@gmail.com>
2019-08-29 15:38:36 +0800

    支持按半个月份分区 ，每半个月一个分片，即每个月的1到15号为一个分片，其余的为另一个分片。并且循环覆盖

--- src/main/java/io/mycat/route/function/PartitionByHalfMonth.java
+++ src/main/java/io/mycat/route/function/PartitionByHalfMonth.java
@@ -1,22 +1,38 @@
 package io.mycat.route.function;
 
 import io.mycat.config.model.rule.RuleAlgorithm;
+import io.mycat.util.StringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.*;
-
-import io.mycat.util.StringUtil;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.List;
 
 /**
- * 例子 按月份列分区 ，每个自然月一个分片，格式 between操作解析的范例
+ * 例子 按半个月份分区 ，每半个月一个分片，即每个月的1到15号为一个分片，其余的为另一个分片
+ *
+ * 如果第一个分片是1月1日，那么每日对应的分片索引如下：
+ *  0 - 1月 1-15, 7月 1-15
+ *  1 - 1月 16-31, 7月 16-31
+ *  2 - 2月 1-15, 8月 1-15
+ *  3 - 2月 16-28/29, 8月 16-31
+ *  4 - 3月 1-15, 9月 1-15
+ *  5 - 3月 16-31, 9月 16-30
+ *  6 - 4月 1-15, 10月 1-15
+ *  7 - 4月 16-30, 10月 16-31
+ *  8 - 5月 1-15, 11月 1-15
+ *  9 - 5月 16-31, 11月 16-30
+ * 10 - 6月 1-15, 12月 1-15
+ * 11 - 6月 16-30, 12月 16-31
  *
  * @author wzh
  *
  */
-public class PartitionByMonth extends AbstractPartitionAlgorithm implements
+public class PartitionByHalfMonth extends AbstractPartitionAlgorithm implements
 		RuleAlgorithm {
 	private static final Logger LOGGER = LoggerFactory.getLogger(PartitionByDate.class);
 	private String sBeginDate;
@@ -58,13 +74,13 @@ public class PartitionByMonth extends AbstractPartitionAlgorithm implements
 //						+ endDate.get(Calendar.MONTH) - beginDate.get(Calendar.MONTH)) + 1;
 				nPartition = 12;
 				if (nPartition <= 0) {
-					throw new java.lang.IllegalArgumentException("Incorrect time range for month partitioning!");
+					throw new IllegalArgumentException("Incorrect time range for month partitioning!");
 				}
 			} else {
 				nPartition = 12;
 			}
 		} catch (ParseException e) {
-			throw new java.lang.IllegalArgumentException(e);
+			throw new IllegalArgumentException(e);
 		}
 	}
 
@@ -101,13 +117,20 @@ public class PartitionByMonth extends AbstractPartitionAlgorithm implements
 			if (scene == 1) {
 				Calendar curTime = Calendar.getInstance();
 				curTime.setTime(formatter.get().parse(columnValue));
-				return curTime.get(Calendar.MONTH);
+				// 月份 从0开始，0表示1月
+				int month = curTime.get(Calendar.MONTH);
+				// 日期
+				int day = curTime.get(Calendar.DAY_OF_MONTH);
+				int targetPartition = month * 2 + (day>15 ? 1 : 0);
+				targetPartition = reCalculatePartition(targetPartition);
+				return targetPartition;
 			}
 			int targetPartition;
 			Calendar curTime = Calendar.getInstance();
 			curTime.setTime(formatter.get().parse(columnValue));
-			// 计算当期时间离开始时间相隔多少个月
-			targetPartition = ((curTime.get(Calendar.YEAR) - beginDate.get(Calendar.YEAR))* 12 + curTime.get(Calendar.MONTH)- beginDate.get(Calendar.MONTH));
+			// 计算当期时间离开始时间相隔多少个半月
+			targetPartition = ( (curTime.get(Calendar.YEAR) - beginDate.get(Calendar.YEAR))* 24 + curTime.get(Calendar.MONTH) * 2 + (curTime.get(Calendar.DAY_OF_MONTH)>15 ? 1 : 0) ) - ( beginDate.get(Calendar.MONTH) * 2 + (beginDate.get(Calendar.DAY_OF_MONTH)>15 ? 1 : 0) );
+
 			/**
 			 * 对于循环分区，需要旋转目标分区的计算值以适合分区范围
 			 */
@@ -141,17 +164,13 @@ public class PartitionByMonth extends AbstractPartitionAlgorithm implements
 		if(partitionTime.before(beginDate)){
 			return new Integer[0];
 		}
-		startPartition = ((partitionTime.get(Calendar.YEAR) - beginDate.get(Calendar.YEAR))
-				* 12 + partitionTime.get(Calendar.MONTH)
-				- beginDate.get(Calendar.MONTH));
+		startPartition = ( (partitionTime.get(Calendar.YEAR) - beginDate.get(Calendar.YEAR))* 24 + partitionTime.get(Calendar.MONTH) * 2 + (partitionTime.get(Calendar.DAY_OF_MONTH)>15 ? 1 : 0) ) - ( beginDate.get(Calendar.MONTH) * 2 + (beginDate.get(Calendar.DAY_OF_MONTH)>15 ? 1 : 0) );
 
 		partitionTime.setTime(format.parse(endValue));
 		if(partitionTime.before(beginDate)){
 			return new Integer[0];
 		}
-		endPartition = ((partitionTime.get(Calendar.YEAR) - beginDate.get(Calendar.YEAR))
-				* 12 + partitionTime.get(Calendar.MONTH)
-				- beginDate.get(Calendar.MONTH));
+		endPartition = ( (partitionTime.get(Calendar.YEAR) - beginDate.get(Calendar.YEAR))* 24 + partitionTime.get(Calendar.MONTH) * 2 + (partitionTime.get(Calendar.DAY_OF_MONTH)>15 ? 1 : 0) ) - ( beginDate.get(Calendar.MONTH) * 2 + (beginDate.get(Calendar.DAY_OF_MONTH)>15 ? 1 : 0) );
 
 		int[] partitionArr = new int[nPartition];
 		List<Integer> list = new ArrayList<>();
